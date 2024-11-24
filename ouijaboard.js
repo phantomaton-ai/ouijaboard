f23eb0fd7dd3ce2a4365c40c6eeffc381499373c
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-11-23 16:11:24 -0800

    tweaks

--- ouijaboard.js
+++ ouijaboard.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import marked from 'marked';
+import { marked } from 'marked';
 import necronomicon from 'necronomicon';
 
 const DEFAULTS = {
@@ -51,7 +51,6 @@ class Ouijaboard {
   }
 
   list() {
-    // List post IDs (no directory, no .md)
     return fs.readdirSync(this.drafts)
       .filter(f => f.endsWith('.md'))
       .map(f => path.parse(f).name);
@@ -62,39 +61,24 @@ class Ouijaboard {
   }
 
   write(post, content) {
-    // Create or overwrite .md file contents, then render .html to renders
-    const draftPath = path.join(this.drafts, `${post}.md`);
-    fs.writeFileSync(draftPath, content);
-    const renderedPath = this.render(post, content);
-    return renderedPath;
+    const draft = path.join(this.drafts, `${post}.md`);
+    fs.writeFileSync(draft, content);
+    const executed = this.spellbook.execute(content);
+    const rendered = marked.parse(executed);
+    const render = path.join(this.renders, `${post}.html`);
+    fs.writeFileSync(render, this.template(rendered));
   }
 
   replace(post, from, to, content) {
-    // Replace post content based on text matching from/to, inclusive (and render)
     const currentContent = this.read(post);
     const newContent = currentContent.replace(new RegExp(from, 'g'), to, content);
     return this.write(post, newContent);
   }
 
   publish(post) {
-    // Copy latest render to publications, fire some hook
-    const renderPath = path.join(this.renders, `${post}.html`);
-    const publicationPath = path.join(this.publications, `${post}.html`);
-    fs.copyFileSync(renderPath, publicationPath);
-    this.firePublishHook(post);
-  }
-
-  render(post, content) {
-    // Render .html to renders directory
-    const renderedContent = this.spellbook.execute(content);
-    const renderedPath = path.join(this.renders, `${post}.html`);
-    fs.writeFileSync(renderedPath, this.template(renderedContent));
-    return renderedPath;
-  }
-
-  firePublishHook(post) {
-    // TODO: Implement any necessary publish hooks
-    console.log(`Published post: ${post}`);
+    const render = path.join(this.renders, `${post}.html`);
+    const publication = path.join(this.publications, `${post}.html`);
+    fs.copyFileSync(render, publication);
   }
 }
 
